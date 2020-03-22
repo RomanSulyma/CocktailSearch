271a5a2d03ccdb1cd57c6b1fe9d2575e2cb02f69
Roman <r.sulima@finik.pro>
2020-03-22 17:52:29 +0200

    Small fixes

--- src/js/controller/LikesListController.js
+++ src/js/controller/LikesListController.js
@@ -6,13 +6,11 @@ export const likesListController = () => {
     //clear likesList UI
     likesListView.deleteLikesList();
     // if true add to likesList else delete from likesList
-    console.log(state.likesList.isLiked(state.recipe));
     if(state.likesList.isLiked(state.recipe)) {
         state.likesList.deleteItem(state.recipe);
     } else {
         state.likesList.addItem(state.recipe)
     }
-    console.log(state.likesList.isLiked(state.recipe));
     //renderList
     likesListView.renderLikesList(state.likesList.list);
     //render like button
